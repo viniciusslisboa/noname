4b2a9ca794f86d5088b444702cfc2defc74c0755
viniciusslisboa <v.soares.lisboa@gmail.com>
2024-03-04 11:06:22 -0300

    add features / abcenses

--- src/scrapping_faltas.py
+++ src/scrapping_faltas.py
@@ -7,24 +7,33 @@ from selenium.webdriver.support.relative_locator import locate_with
 def scrappFaltas(matricula, senha): 
   options = webdriver.ChromeOptions()
   options.add_argument("--headless")
-  driver = webdriver.Chrome(options=options)
+  driver = webdriver.Chrome(options=options) 
   driver.get("https://academico.iff.edu.br/qacademico/index.asp?t=1001")
-
-  
 
   driver.find_element(By.NAME, "LOGIN").send_keys(matricula)
   driver.find_element(By.NAME, "SENHA").send_keys(senha)
   driver.find_element(By.CLASS_NAME, "btnOk").click()
 
-  driver.get("https://academico.iff.edu.br/qacademico/index.asp?t=2071")
-
   time.sleep(1)
 
-  td = driver.find_elements(By.XPATH, "//tr/td[contains(text(), 'Faltas')]/following-sibling::td")
+  driver.find_element(By.XPATH, "/html/body/table/tbody/tr[2]/td/table/tbody/tr[2]/td[2]/table/tbody/tr[4]/td[3]/a").click()
 
-  absences = 0
-  for el in td:
-    absences += int(el.text)
+  faltas = driver.find_elements(By.XPATH, "//tr/td[contains(text(), 'Faltas')]/following-sibling::td")
+  aulas = driver.find_elements(By.XPATH, "//tbody/tr/td[contains(text(), 'Aulas ministradas')]/following-sibling::td/a[1]")
 
-  print(f'O usuário possui {absences} faltas.') 
+  numFaltas = 0
+  numeroAulas = 0
+
+  for el in faltas:
+    numFaltas += int(el.text)
+  
+  for el in aulas:
+    numeroAulas += int(el.text)
+
+  # numFaltas -= 41
+  porcentFaltas = (numFaltas * 100) / numeroAulas
+
+  print(f"{round(porcentFaltas, 2)}%")
+
+
 
